d9d5d46bd8dfedf27469ee6da6026a54ff068180
Andreas Herz <a.herz@freegroup.de>
2023-01-02 10:35:51 +0100

    -empty-

---
+++ data/global/digital/buttons/4-Bit-Switch.js
@@ -0,0 +1,163 @@
+// Generated Code for the Draw2D touch HTML5 lib.
+// File will be generated if you save the *.shape file.
+//
+// created with http://www.draw2d.org
+//
+//
+var digital_buttons_4_Bit_Switch = CircuitFigure.extend({
+
+   NAME: "digital_buttons_4_Bit_Switch",
+   VERSION: "local-version",
+
+   init:function(attr, setter, getter)
+   {
+     var _this = this;
+
+     this._super( $.extend({stroke:0, bgColor:null, width:20,height:160},attr), setter, getter);
+     var port;
+     // port01
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 5.93131249999999 }));
+     port.setConnectionDirection(1);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port01");
+     port.setMaxFanOut(20);
+     // port02
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 105, y: 19.0625 }));
+     port.setConnectionDirection(1);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port02");
+     port.setMaxFanOut(20);
+     // port03
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 31.25 }));
+     port.setConnectionDirection(1);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port03");
+     port.setMaxFanOut(20);
+     // port04
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 43.75 }));
+     port.setConnectionDirection(1);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port04");
+     port.setMaxFanOut(20);
+     // port05
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 56.434937500000046 }));
+     port.setConnectionDirection(1);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port05");
+     port.setMaxFanOut(20);
+     // port06
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 68.72787499999959 }));
+     port.setConnectionDirection(1);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port06");
+     port.setMaxFanOut(20);
+     // port07
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 105, y: 81.1621875000003 }));
+     port.setConnectionDirection(1);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port07");
+     port.setMaxFanOut(20);
+     // port08
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 105, y: 93.93256250000036 }));
+     port.setConnectionDirection(1);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port08");
+     port.setMaxFanOut(20);
+   },
+
+   createShapeElement : function()
+   {
+      var shape = this._super();
+      this.originalWidth = 20;
+      this.originalHeight= 160;
+      return shape;
+   },
+
+   createSet: function()
+   {
+       this.canvas.paper.setStart();
+       var shape = null;
+       // BoundingBox
+       shape = this.canvas.paper.path("M0,0 L20,0 L20,160 L0,160");
+       shape.attr({"stroke":"none","stroke-width":0,"fill":"none"});
+       shape.data("name","BoundingBox");
+       
+       // rect01
+       shape = this.canvas.paper.path('M20 20L0 20L0 0L20 0Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","rect01");
+       
+       // rect02
+       shape = this.canvas.paper.path('M20 40L0 40L0 20L20 20Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","rect02");
+       
+       // rect03
+       shape = this.canvas.paper.path('M20 60L0 60L0 40L20 40Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","rect03");
+       
+       // rect04
+       shape = this.canvas.paper.path('M20 80L0 80L0 60L20 60Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","rect04");
+       
+       // rect05
+       shape = this.canvas.paper.path('M20 100L0 100L0 80L20 80Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","rect05");
+       
+       // rect06
+       shape = this.canvas.paper.path('M20 120L0 120L0 100L20 100Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","rect06");
+       
+       // rect07
+       shape = this.canvas.paper.path('M20 140L0 140L0 120L20 120Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","rect07");
+       
+       // rect08
+       shape = this.canvas.paper.path('M20 160L0 160L0 140L20 140Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","rect08");
+       
+
+       return this.canvas.paper.setFinish();
+   }
+});
+
+/**
+ * by 'Draw2D Shape Designer'
+ *
+ * Custom JS code to tweak the standard behaviour of the generated
+ * shape. add your custome code and event handler here.
+ *
+ *
+ */
+digital_buttons_4_Bit_Switch = digital_buttons_4_Bit_Switch.extend({
+
+    init: function(attr, setter, getter){
+         this._super(attr, setter, getter);
+
+
+        this.setResizeable(false);
+        this.installEditPolicy(new draw2d.policy.figure.AntSelectionFeedbackPolicy());
+
+        this.on("click",function(emitter, event){
+            var h = emitter.getHeight();
+            var modh = h/8;
+            var index = (event.relY/modh)|0;
+            var port = emitter.getOutputPort(index);
+            port.setValue(!port.getBooleanValue());
+            emitter.layerAttr("rect0"+(index+1), {fill:port.getBooleanValue()?"#C21B7A":null});
+        });
+    },
+    
+    calculate: function()
+    {
+    
+        
+    }
+
+});
---
+++ data/global/digital/buttons/4-Bit-Switch.md
@@ -0,0 +1,5 @@
+# High / Low Array
+
+The High Low Array is a macro object for 8 toggle switches in row.
+
+
