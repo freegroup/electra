d11b34b579150be21d5103c8f2da68dfa8b0ef9b
Andreas Herz <a.herz@freegroup.de>
2023-01-02 10:37:56 +0100

    -empty-

--- data/global/digital/buttons/4-Bit-Switch.js
+++ data/global/digital/buttons/4-Bit-Switch.js
@@ -13,55 +13,31 @@ var digital_buttons_4_Bit_Switch = CircuitFigure.extend({
    {
      var _this = this;
 
-     this._super( $.extend({stroke:0, bgColor:null, width:20,height:160},attr), setter, getter);
+     this._super( $.extend({stroke:0, bgColor:null, width:20,height:80},attr), setter, getter);
      var port;
      // port01
-     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 5.93131249999999 }));
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 11.86262499999998 }));
      port.setConnectionDirection(1);
      port.setBackgroundColor("#37B1DE");
      port.setName("port01");
      port.setMaxFanOut(20);
      // port02
-     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 105, y: 19.0625 }));
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 105, y: 38.125 }));
      port.setConnectionDirection(1);
      port.setBackgroundColor("#37B1DE");
      port.setName("port02");
      port.setMaxFanOut(20);
      // port03
-     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 31.25 }));
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 62.5 }));
      port.setConnectionDirection(1);
      port.setBackgroundColor("#37B1DE");
      port.setName("port03");
      port.setMaxFanOut(20);
      // port04
-     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 43.75 }));
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 87.5 }));
      port.setConnectionDirection(1);
      port.setBackgroundColor("#37B1DE");
      port.setName("port04");
-     port.setMaxFanOut(20);
-     // port05
-     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 56.434937500000046 }));
-     port.setConnectionDirection(1);
-     port.setBackgroundColor("#37B1DE");
-     port.setName("port05");
-     port.setMaxFanOut(20);
-     // port06
-     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 102.5, y: 68.72787499999959 }));
-     port.setConnectionDirection(1);
-     port.setBackgroundColor("#37B1DE");
-     port.setName("port06");
-     port.setMaxFanOut(20);
-     // port07
-     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 105, y: 81.1621875000003 }));
-     port.setConnectionDirection(1);
-     port.setBackgroundColor("#37B1DE");
-     port.setName("port07");
-     port.setMaxFanOut(20);
-     // port08
-     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 105, y: 93.93256250000036 }));
-     port.setConnectionDirection(1);
-     port.setBackgroundColor("#37B1DE");
-     port.setName("port08");
      port.setMaxFanOut(20);
    },
 
@@ -69,7 +45,7 @@ var digital_buttons_4_Bit_Switch = CircuitFigure.extend({
    {
       var shape = this._super();
       this.originalWidth = 20;
-      this.originalHeight= 160;
+      this.originalHeight= 80;
       return shape;
    },
 
@@ -78,7 +54,7 @@ var digital_buttons_4_Bit_Switch = CircuitFigure.extend({
        this.canvas.paper.setStart();
        var shape = null;
        // BoundingBox
-       shape = this.canvas.paper.path("M0,0 L20,0 L20,160 L0,160");
+       shape = this.canvas.paper.path("M0,0 L20,0 L20,80 L0,80");
        shape.attr({"stroke":"none","stroke-width":0,"fill":"none"});
        shape.data("name","BoundingBox");
        
@@ -102,26 +78,6 @@ var digital_buttons_4_Bit_Switch = CircuitFigure.extend({
        shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
        shape.data("name","rect04");
        
-       // rect05
-       shape = this.canvas.paper.path('M20 100L0 100L0 80L20 80Z');
-       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
-       shape.data("name","rect05");
-       
-       // rect06
-       shape = this.canvas.paper.path('M20 120L0 120L0 100L20 100Z');
-       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
-       shape.data("name","rect06");
-       
-       // rect07
-       shape = this.canvas.paper.path('M20 140L0 140L0 120L20 120Z');
-       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
-       shape.data("name","rect07");
-       
-       // rect08
-       shape = this.canvas.paper.path('M20 160L0 160L0 140L20 140Z');
-       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
-       shape.data("name","rect08");
-       
 
        return this.canvas.paper.setFinish();
    }
@@ -140,13 +96,14 @@ digital_buttons_4_Bit_Switch = digital_buttons_4_Bit_Switch.extend({
     init: function(attr, setter, getter){
          this._super(attr, setter, getter);
 
-
+        this.bitCount = 4;
+        
         this.setResizeable(false);
         this.installEditPolicy(new draw2d.policy.figure.AntSelectionFeedbackPolicy());
 
-        this.on("click",function(emitter, event){
+        this.on("click",(emitter, event) => {
             var h = emitter.getHeight();
-            var modh = h/8;
+            var modh = h/this.bitCount;
             var index = (event.relY/modh)|0;
             var port = emitter.getOutputPort(index);
             port.setValue(!port.getBooleanValue());
