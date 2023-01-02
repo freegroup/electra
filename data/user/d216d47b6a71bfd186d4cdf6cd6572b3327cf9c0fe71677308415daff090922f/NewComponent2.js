580c70695fb400e4fc72e760b235ab60a29c521e
Andreas Herz <a.herz@freegroup.de>
2023-01-02 18:10:47 +0100

    -empty-

---
+++ data/user/d216d47b6a71bfd186d4cdf6cd6572b3327cf9c0fe71677308415daff090922f/NewComponent2.js
@@ -0,0 +1,105 @@
+// Generated Code for the Draw2D touch HTML5 lib.
+// File will be generated if you save the *.shape file.
+//
+// created with http://www.draw2d.org
+//
+//
+var NewComponent2 = CircuitFigure.extend({
+
+   NAME: "NewComponent2",
+   VERSION: "local-version",
+
+   init:function(attr, setter, getter)
+   {
+     var _this = this;
+
+     this._super( $.extend({stroke:0, bgColor:null, width:51,height:77},attr), setter, getter);
+     var port;
+   },
+
+   createShapeElement : function()
+   {
+      var shape = this._super();
+      this.originalWidth = 51;
+      this.originalHeight= 77;
+      return shape;
+   },
+
+   createSet: function()
+   {
+       this.canvas.paper.setStart();
+       var shape = null;
+       // BoundingBox
+       shape = this.canvas.paper.path("M0,0 L51,0 L51,77 L0,77");
+       shape.attr({"stroke":"none","stroke-width":0,"fill":"none"});
+       shape.data("name","BoundingBox");
+       
+       // Rectangle
+       shape = this.canvas.paper.path('M0 38.95363771846132L0 77L51 77L51 0L42.46164007346033 0L42.85533905932698 4.5L42.31821201600087 10.639390198448382L40.72315121943393 16.59223813239896L38.118621784789866 22.177669529663945L34.58376102098555 27.22597388360191L30.22597388360191 31.58376102098555L25.177669529663945 35.118621784789866L19.59223813239896 37.72315121943393L13.639390198448382 39.31821201600087L7.5 39.85533905932698L1.3606098015516181 39.31821201600087L0 38.95363771846132Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","Rectangle");
+       
+
+       return this.canvas.paper.setFinish();
+   }
+});
+
+/**
+ * Generated Code for the Draw2D touch HTML5 lib.
+ * File will be generated if you save the *.shape file.
+ *
+ * by 'Draw2D Shape Designer'
+ *
+ * Custom JS code to tweak the standard behaviour of the generated
+ * shape. add your custom code and event handler here.
+ *
+ * Looks disconcerting - extending my own class. But this is a good method to
+ * merge basic code and override them with custom methods.
+ */
+NewComponent2 = NewComponent2.extend({
+
+    init: function(attr, setter, getter){
+         this._super(attr, setter, getter);
+
+         // your special code here
+    },
+
+    /**
+     *  Called by the simulator for every calculation
+     *  loop
+     *  @param {Object} context context where objects can store or handover global variables to other objects.
+     *  @required
+     **/
+    calculate:function( context)
+    {
+    },
+
+
+    /**
+     *  Called if the simulation mode is starting
+     *  @required
+     **/
+    onStart:function( context )
+    {
+    },
+
+    /**
+     *  Called if the simulation mode is stopping
+     *  @required
+     **/
+    onStop:function( context )
+    {
+    },
+
+    /**
+     * Get the simulator a hint which kind of hardware the shapes requires or supports
+     * This helps the simulator to bring up some dialogs and messages if any new hardware is connected/get lost
+     * and your are running a circuit which needs this kind of hardware...
+     **/
+    getRequiredHardware: function(){
+      return {
+        arduino: false
+      }
+    }
+
+});
