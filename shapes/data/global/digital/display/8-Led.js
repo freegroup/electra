56d0e79200d8448ad7115a8acd98ee13bb12bf79
Andreas Herz <a.herz@freegroup.de>
2023-01-13 15:59:13 +0100

    -empty-

--- shapes/data/global/digital/display/8-Led.js
+++ shapes/data/global/digital/display/8-Led.js
@@ -13,21 +13,63 @@ var digital_display_8_Led = CircuitFigure.extend({
    {
      var _this = this;
 
-     this._super( $.extend({stroke:0, bgColor:null, width:30,height:32},attr), setter, getter);
+     this._super( $.extend({stroke:0, bgColor:null, width:25,height:200},attr), setter, getter);
      var port;
-     // Port
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -3.3333333333333335, y: 51.5625 }));
+     // port0
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -19.1108000000022, y: 6.25 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
-     port.setName("Port");
+     port.setName("port0");
+     port.setMaxFanOut(20);
+     // port1
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -19.1108000000022, y: 19.165350000000217 }));
+     port.setConnectionDirection(3);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port1");
+     port.setMaxFanOut(20);
+     // port2
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -19.1108000000022, y: 31.503250000000207 }));
+     port.setConnectionDirection(3);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port2");
+     port.setMaxFanOut(20);
+     // port3
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -19.1108000000022, y: 43.496050000000196 }));
+     port.setConnectionDirection(3);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port3");
+     port.setMaxFanOut(20);
+     // port4
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -19.1108000000022, y: 56.25 }));
+     port.setConnectionDirection(3);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port4");
+     port.setMaxFanOut(20);
+     // port5
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -19.1108000000022, y: 69.16535000000022 }));
+     port.setConnectionDirection(3);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port5");
+     port.setMaxFanOut(20);
+     // port6
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -19.1108000000022, y: 81.64410000000044 }));
+     port.setConnectionDirection(3);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port6");
+     port.setMaxFanOut(20);
+     // port7
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -19.1108000000022, y: 94.20030000000042 }));
+     port.setConnectionDirection(3);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("port7");
      port.setMaxFanOut(20);
    },
 
    createShapeElement : function()
    {
       var shape = this._super();
-      this.originalWidth = 30;
-      this.originalHeight= 32;
+      this.originalWidth = 25;
+      this.originalHeight= 200;
       return shape;
    },
 
@@ -36,24 +78,49 @@ var digital_display_8_Led = CircuitFigure.extend({
        this.canvas.paper.setStart();
        var shape = null;
        // BoundingBox
-       shape = this.canvas.paper.path("M0,0 L30,0 L30,32 L0,32");
+       shape = this.canvas.paper.path("M0,0 L25,0 L25,200 L0,200");
        shape.attr({"stroke":"none","stroke-width":0,"fill":"none"});
        shape.data("name","BoundingBox");
        
        // circle
-       shape = this.canvas.paper.ellipse();
-       shape.attr({"rx":15,"ry":16,"cx":15,"cy":16,"stroke":"rgba(27,27,27,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape = this.canvas.paper.path('M0 0L25 0L25 25L0 25Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
        shape.data("name","circle");
        
-       // Line
-       shape = this.canvas.paper.path('M5.522100000000137 5.682400000001508L15.138100000001941,16.496800000000803L24.754100000001927,27.31119999999919');
-       shape.attr({"stroke-linecap":"round","stroke-linejoin":"round","stroke":"rgba(0,0,0,1)","stroke-width":1,"stroke-dasharray":null,"opacity":1});
-       shape.data("name","Line");
+       // led1
+       shape = this.canvas.paper.path('M0 25L25 25L25 50L0 50Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","led1");
        
-       // Line
-       shape = this.canvas.paper.path('M25.94430000000102 5.062700000001314L5.283199999999852,27.963700000000244');
-       shape.attr({"stroke-linecap":"round","stroke-linejoin":"round","stroke":"rgba(0,0,0,1)","stroke-width":1,"stroke-dasharray":null,"opacity":1});
-       shape.data("name","Line");
+       // led2
+       shape = this.canvas.paper.path('M0 50L25 50L25 75L0 75Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","led2");
+       
+       // led3
+       shape = this.canvas.paper.path('M0 75L25 75L25 100L0 100Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","led3");
+       
+       // led4
+       shape = this.canvas.paper.path('M0 100L25 100L25 125L0 125Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","led4");
+       
+       // led5
+       shape = this.canvas.paper.path('M0 125L25 125L25 150L0 150Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","led5");
+       
+       // led6
+       shape = this.canvas.paper.path('M0 150L25 150L25 175L0 175Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","led6");
+       
+       // led7
+       shape = this.canvas.paper.path('M0 175L25 175L25 200L0 200Z');
+       shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
+       shape.data("name","led7");
        
 
        return this.canvas.paper.setFinish();
