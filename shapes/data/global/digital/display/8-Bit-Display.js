3c6a7676889fac5d0ba43bc805204e59a6853d66
Andreas Herz <a.herz@freegroup.de>
2023-01-16 19:39:25 +0100

    -empty-

--- shapes/data/global/digital/display/8-Bit-Display.js
+++ shapes/data/global/digital/display/8-Bit-Display.js
@@ -13,57 +13,57 @@ var digital_display_8_Bit_Display = CircuitFigure.extend({
    {
      var _this = this;
 
-     this._super( $.extend({stroke:0, bgColor:null, width:240,height:152},attr), setter, getter);
+     this._super( $.extend({stroke:0, bgColor:null, width:189,height:137},attr), setter, getter);
      var port;
-     // port_a
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 11.513157894736842 }));
+     // input_bit1
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 6.204379562043796 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
-     port.setName("port_a");
-     port.setMaxFanOut(20);
-     // port_b
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 24.67105263157895 }));
+     port.setName("input_bit1");
+     port.setMaxFanOut(1);
+     // input_bit2
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 20.43795620437956 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
-     port.setName("port_b");
-     port.setMaxFanOut(20);
-     // port_c
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 37.828947368421055 }));
+     port.setName("input_bit2");
+     port.setMaxFanOut(1);
+     // input_bit3
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 35.03649635036496 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
-     port.setName("port_c");
-     port.setMaxFanOut(20);
-     // port_d
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 50.986842105263165 }));
+     port.setName("input_bit3");
+     port.setMaxFanOut(1);
+     // input_bit4
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 50 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
-     port.setName("port_d");
-     port.setMaxFanOut(20);
-     // port_e
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 64.14473684210527 }));
+     port.setName("input_bit4");
+     port.setMaxFanOut(1);
+     // input_bit5
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 64.5985401459854 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
-     port.setName("port_e");
-     port.setMaxFanOut(20);
-     // port_f
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 77.30263157894737 }));
+     port.setName("input_bit5");
+     port.setMaxFanOut(1);
+     // input_bit6
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 79.1970802919708 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
-     port.setName("port_f");
-     port.setMaxFanOut(20);
-     // port_g
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 90.46052631578948 }));
+     port.setName("input_bit6");
+     port.setMaxFanOut(1);
+     // input_bit7
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 93.7956204379562 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
-     port.setName("port_g");
-     port.setMaxFanOut(20);
+     port.setName("input_bit7");
+     port.setMaxFanOut(1);
    },
 
    createShapeElement : function()
    {
       var shape = this._super();
-      this.originalWidth = 240;
-      this.originalHeight= 152;
+      this.originalWidth = 189;
+      this.originalHeight= 137;
       return shape;
    },
 
@@ -72,18 +72,18 @@ var digital_display_8_Bit_Display = CircuitFigure.extend({
        this.canvas.paper.setStart();
        var shape = null;
        // BoundingBox
-       shape = this.canvas.paper.path("M0,0 L240,0 L240,152 L0,152");
+       shape = this.canvas.paper.path("M0,0 L189,0 L189,137 L0,137");
        shape.attr({"stroke":"none","stroke-width":0,"fill":"none"});
        shape.data("name","BoundingBox");
        
        // circle
-       shape = this.canvas.paper.path('M0,2Q0,0 2, 0L238,0Q240,0 240, 2L240,150Q240,152 238, 152L2,152Q0,152 0, 150L0,2');
+       shape = this.canvas.paper.path('M0,2Q0,0 2, 0L187,0Q189,0 189, 2L189,135Q189,137 187, 137L2,137Q0,137 0, 135L0,2');
        shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
        shape.data("name","circle");
        
        // digit
        shape = this.canvas.paper.text(0,0,'255');
-       shape.attr({"x":18.890625,"y":75,"text-anchor":"start","text":"255","font-family":"\"Arial\"","font-size":120,"stroke":"#000000","fill":"#080808","stroke-scale":true,"font-weight":"normal","stroke-width":0,"opacity":1});
+       shape.attr({"x":15.890625,"y":68.75,"text-anchor":"start","text":"255","font-family":"\"Arial\"","font-size":100,"stroke":"#000000","fill":"#080808","stroke-scale":true,"font-weight":"normal","stroke-width":0,"opacity":1});
        shape.data("name","digit");
        
 
@@ -108,13 +108,11 @@ digital_display_8_Bit_Display = digital_display_8_Bit_Display.extend({
     
     calculate:function(context)
     {
-        var _this = this;
-        this.portLockup.forEach(function(element, index){
-            _this.layerAttr(element.s, { 
-                fill  : element.p.getBooleanValue()?"#C21B7A":null,
-                stroke: element.p.getBooleanValue()?"#000000":"#f4f4f9"
-            });
-        });
+        let val = this.getInputPort("input_a1").getBooleanValue()?1:0;
+        val    += this.getInputPort("input_a2").getBooleanValue()?2:0;
+        val    += this.getInputPort("input_a3").getBooleanValue()?4:0;
+        val    += this.getInputPort("input_a4").getBooleanValue()?8:0;
+
     }
 
 });
