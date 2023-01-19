5bf3599a33e05d0e5f40439a4a95d8555ba3539c
Andreas Herz <a.herz@freegroup.de>
2023-01-19 15:24:12 +0100

    -empty-

--- shapes/data/global/digital/display/4-Bit-Display.js
+++ shapes/data/global/digital/display/4-Bit-Display.js
@@ -13,28 +13,28 @@ var digital_display_4_Bit_Display = CircuitFigure.extend({
    {
      var _this = this;
 
-     this._super( $.extend({stroke:0, bgColor:null, width:96.984375,height:97.5},attr), setter, getter);
+     this._super( $.extend({stroke:0, bgColor:null, width:97,height:97.5},attr), setter, getter);
      var port;
      // input_bit1
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 20.34912820512825 }));
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 1.0309278350515463, y: 19.323487179487223 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
      port.setName("input_bit1");
      port.setMaxFanOut(1);
      // input_bit2
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 40.86194871794876 }));
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 1.0309278350515463, y: 39.836307692307734 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
      port.setName("input_bit2");
      port.setMaxFanOut(1);
      // input_bit3
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 61.374769230769274 }));
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 1.0309278350515463, y: 60.349128205128245 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
      port.setName("input_bit3");
      port.setMaxFanOut(1);
      // input_bit4
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 0, y: 81.88758974358979 }));
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 1.0309278350515463, y: 80.86194871794876 }));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#37B1DE");
      port.setName("input_bit4");
@@ -44,7 +44,7 @@ var digital_display_4_Bit_Display = CircuitFigure.extend({
    createShapeElement : function()
    {
       var shape = this._super();
-      this.originalWidth = 96.984375;
+      this.originalWidth = 97;
       this.originalHeight= 97.5;
       return shape;
    },
@@ -54,18 +54,18 @@ var digital_display_4_Bit_Display = CircuitFigure.extend({
        this.canvas.paper.setStart();
        var shape = null;
        // BoundingBox
-       shape = this.canvas.paper.path("M0,0 L96.984375,0 L96.984375,97.5 L0,97.5");
+       shape = this.canvas.paper.path("M0,0 L97,0 L97,97.5 L0,97.5");
        shape.attr({"stroke":"none","stroke-width":0,"fill":"none"});
        shape.data("name","BoundingBox");
        
        // circle
-       shape = this.canvas.paper.path('M0,7.840400000000045Q0,5.840400000000045 2, 5.840400000000045L94,5.840400000000045Q96,5.840400000000045 96, 7.840400000000045L96,90.84040000000005Q96,92.84040000000005 94, 92.84040000000005L2,92.84040000000005Q0,92.84040000000005 0, 90.84040000000005L0,7.840400000000045');
+       shape = this.canvas.paper.path('M1,6.840400000000045Q1,4.840400000000045 3, 4.840400000000045L95,4.840400000000045Q97,4.840400000000045 97, 6.840400000000045L97,89.84040000000005Q97,91.84040000000005 95, 91.84040000000005L3,91.84040000000005Q1,91.84040000000005 1, 89.84040000000005L1,6.840400000000045');
        shape.attr({"stroke":"rgba(48,48,48,1)","stroke-width":1,"fill":"rgba(255,255,255,1)","dasharray":null,"stroke-dasharray":null,"opacity":1});
        shape.data("name","circle");
        
        // digit
-       shape = this.canvas.paper.text(0,0,'16');
-       shape.attr({"x":4,"y":48.75,"text-anchor":"start","text":"16","font-family":"\"Arial\"","font-size":80,"stroke":"#000000","fill":"#D11131","stroke-scale":true,"font-weight":"normal","stroke-width":0,"opacity":1});
+       shape = this.canvas.paper.text(0,0,'15');
+       shape.attr({"x":4,"y":48.75,"text-anchor":"start","text":"15","font-family":"\"Arial\"","font-size":80,"stroke":"#000000","fill":"#D11131","stroke-scale":true,"font-weight":"normal","stroke-width":0,"opacity":1});
        shape.data("name","digit");
        
 
