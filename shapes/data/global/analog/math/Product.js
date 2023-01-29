6c7402e3d65ce7e195adb3e4e2b80ce8a285ad07
Andreas Herz <a.herz@freegroup.de>
2023-01-29 14:22:53 +0100

    -empty-

--- shapes/data/global/analog/math/Product.js
+++ shapes/data/global/analog/math/Product.js
@@ -13,19 +13,25 @@ var analog_math_Product = CircuitFigure.extend({
    {
      var _this = this;
 
-     this._super( $.extend({stroke:0, bgColor:null, width:40,height:40.390625},attr), setter, getter);
+     this._super( $.extend({stroke:0, bgColor:null, width:40,height:40},attr), setter, getter);
      var port;
-     // input_port1
-     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -2.5, y: 52.44197292069633 }));
-     port.setConnectionDirection(3);
-     port.setBackgroundColor("#37B1DE");
-     port.setName("input_port1");
-     port.setMaxFanOut(20);
-     // output_port1
-     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 105, y: 51.992263056092845 }));
+     // output
+     port = this.addPort(new DecoratedOutputPort(), new draw2d.layout.locator.XYRelPortLocator({x: 105, y: 52.5 }));
      port.setConnectionDirection(1);
      port.setBackgroundColor("#37B1DE");
-     port.setName("output_port1");
+     port.setName("output");
+     port.setMaxFanOut(20);
+     // input_1
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -2.5, y: 25.48828125 }));
+     port.setConnectionDirection(3);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("input_1");
+     port.setMaxFanOut(20);
+     // input_2
+     port = this.addPort(new DecoratedInputPort(), new draw2d.layout.locator.XYRelPortLocator({x: -2.5, y: 75.48828125 }));
+     port.setConnectionDirection(3);
+     port.setBackgroundColor("#37B1DE");
+     port.setName("input_2");
      port.setMaxFanOut(20);
    },
 
@@ -33,7 +39,7 @@ var analog_math_Product = CircuitFigure.extend({
    {
       var shape = this._super();
       this.originalWidth = 40;
-      this.originalHeight= 40.390625;
+      this.originalHeight= 40;
       return shape;
    },
 
@@ -42,7 +48,7 @@ var analog_math_Product = CircuitFigure.extend({
        this.canvas.paper.setStart();
        var shape = null;
        // BoundingBox
-       shape = this.canvas.paper.path("M0,0 L40,0 L40,40.390625 L0,40.390625");
+       shape = this.canvas.paper.path("M0,0 L40,0 L40,40 L0,40");
        shape.attr({"stroke":"none","stroke-width":0,"fill":"none"});
        shape.data("name","BoundingBox");
        
@@ -53,13 +59,8 @@ var analog_math_Product = CircuitFigure.extend({
        
        // Label
        shape = this.canvas.paper.text(0,0,'X');
-       shape.attr({"x":12,"y":23.390625,"text-anchor":"start","text":"X","font-family":"\"Arial\"","font-size":23,"stroke":"#000000","fill":"#080808","stroke-scale":true,"font-weight":"normal","stroke-width":0,"opacity":1});
+       shape.attr({"x":12.5,"y":20.390625,"text-anchor":"start","text":"X","font-family":"\"Arial\"","font-size":23,"stroke":"#000000","fill":"#080808","stroke-scale":true,"font-weight":"normal","stroke-width":0,"opacity":1});
        shape.data("name","Label");
-       
-       // Line
-       shape = this.canvas.paper.path('M28.405000000001564 9.709000000001652L20.725000000002183,9.709000000001652L13.045000000001892,9.709000000001652L12.02100000000155,9.709000000001652');
-       shape.attr({"stroke-linecap":"round","stroke-linejoin":"round","stroke":"rgba(0,0,0,1)","stroke-width":2,"stroke-dasharray":null,"opacity":1});
-       shape.data("name","Line");
        
 
        return this.canvas.paper.setFinish();
@@ -82,29 +83,6 @@ analog_math_Product = analog_math_Product.extend({
 
     init: function(attr, setter, getter){
         this._super(attr, setter, getter);
-
-        this.total   = 0;    // the running total
-        this.average = 0;    // the average
-        this.readings = []; 
-        this.readIndex = 0;
-        this.numReadings =0;
-        
-        this.on("change:userData.numReadings",(emitter, event)=>{
-            this.numReadings = parseInt(event.value);
-            this.readings = []; 
-            this.total =0;
-            this.average = 0;
-            this.readings.length = this.numReadings;
-            this.pointer=0;
-            this.readIndex = 0;
-            this.readings.fill(0)
-        });
-        
-        this.attr({
-            resizeable:false,
-            "userData.numReadings":10
-        });
-
         this.installEditPolicy(new draw2d.policy.figure.AntSelectionFeedbackPolicy());
     },
 
@@ -116,42 +94,8 @@ analog_math_Product = analog_math_Product.extend({
      **/
     calculate:function( context)
     {
-        this.total = this.total - this.readings[this.readIndex];
-        this.readings[this.readIndex] = this.getInputPort(0).getValue() || 0;
-        this.total = this.total + this.readings[this.readIndex];
-        this.readIndex = (this.readIndex+1)%this.readings.length;
-        this.average = parseInt((this.total / this.readings.length)*1000)/1000;
-        this.getOutputPort(0).setValue(this.average);
-    },
-
-    /**
-     *  Called if the simulation mode is starting
-     *  @required
-     **/
-    onStart:function( context )
-    {
-    },
-
-    /**
-     *  Called if the simulation mode is stopping
-     *  @required
-     **/
-    onStop:function( context )
-    {
-    },
-    
-    getParameterSettings:function()
-    {
-        return [
-        {
-            name:"numReadings",
-            label:"Number of Readings for the average",
-            property:{
-                type: "integer",
-                min: 10,
-                max: 150,
-                increment:1
-        }
-        }];
+        let v1 = this.getInputPort(0).getValue() ?? 0;
+        let v2 = this.getInputPort(1).getValue() ?? 0;
+        this.getOutputPort(0).setValue(v1*v2);
     }
 });
