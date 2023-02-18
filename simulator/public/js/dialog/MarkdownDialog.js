4ac1519d01dddf01c978453a440e965d6e8311c1
man from earth <1155039+freegroup@users.noreply.github.com>
2023-02-18 13:36:19 +0100

    reove the "edit documetnation" in the documentation editor. is not part of the simulator anymore, because "normal" user can't edit the docu

--- simulator/public/js/dialog/MarkdownDialog.js
+++ simulator/public/js/dialog/MarkdownDialog.js
@@ -9,14 +9,6 @@ import conf from "../Configuration"
 class Dialog {
 
   constructor() {
-    this.defaults = {
-      html: true,             // Enable HTML tags in source
-      xhtmlOut: false,        // Use '/' to close single tags (<br />)
-      breaks: false,          // Convert '\n' in paragraphs into <br>
-      langPrefix: 'language-',// CSS language prefix for fenced blocks
-      linkTarget: '_blank',   // set target to open link in
-      typographer: true       // Enable smartypants and other sweet transforms
-    }
   }
 
   show(figure) {
@@ -29,36 +21,8 @@ class Dialog {
     $.get(contentUrl,  (content) => {
       $('#markdownDialog .markdownRendering').html(md.render(content))
       $('#markdownDialog .media-heading').html(displayName)
-      if(figure instanceof CircuitFigure){
-        $('#markdownDialog .editButton').show()
-      }
-      else{
-        $('#markdownDialog .editButton').hide()
-      }
-
       $('#markdownDialog').modal('show')
-
-      $("#markdownDialog .editButton").off("click").on("click", () => {
-        let designerUrl = `../designer?${scope}=${shapeName}`
-        window.open(designerUrl, "designer")
-      })
-
-      $("#markdownDialog .editButtonGuided").off("click").on("click", () => {
-        let designerUrl = `../designer?${scope}=${shapeName}?tutorial=markdown`
-        window.open(designerUrl, "designer")
-      })
     })
-  }
-
-  validateLink(url) {
-    let BAD_PROTOCOLS = [ 'vbscript', 'javascript', 'file'];
-    let str = url.trim().toLowerCase();
-    // Care about digital entities "javascript&#x3A;alert(1)"
-    str = utils.replaceEntities(str);
-    if (str.indexOf(':') !== -1 && BAD_PROTOCOLS.indexOf(str.split(':')[0]) !== -1) {
-      return false;
-    }
-    return true;
   }
 }
 
