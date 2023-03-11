c4b37bebee4bb47645455f863037a7a8dbdeaafb
man from earth <1155039+freegroup@users.noreply.github.com>
2023-03-11 09:15:49 +0100

    ...

--- common/public/js/loadScript.js
+++ common/public/js/loadScript.js
@@ -1,21 +1,22 @@
 export default function(src) {
     return new Promise(function(resolve, reject) {
-      const s = document.createElement('script');
-      let r = false;
-      s.type = 'text/javascript';
-      s.src = src;
-      s.async = true;
+      const s = document.createElement('script')
+      let r = false
+      s.type = 'text/javascript'
+      s.src = src
+      s.async = true
+      s.crossorigin = true
       s.onerror = function(err) {
-        reject(err, s);
-      };
+        reject(err, s)
+      }
       s.onload = s.onreadystatechange = function() {
         // console.log(this.readyState); // uncomment this line to see which ready states are called.
         if (!r && (!this.readyState || this.readyState == 'complete')) {
-          r = true;
-          resolve();
+          r = true
+          resolve()
         }
       };
-      const t = document.getElementsByTagName('script')[0];
-      t.parentElement.insertBefore(s, t);
+      const t = document.getElementsByTagName('script')[0]
+      t.parentElement.insertBefore(s, t)
     });
   }
